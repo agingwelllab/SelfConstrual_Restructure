15d9e07bfced438bdf0c6c7c424d2bc0142bd106
sspill <ssp160230@utdallas.edu>
2021-09-28 14:23:33 -0500

    modified based on pull requests for other repos

--- clean_IndCol_data.R
+++ clean_IndCol_data.R
@@ -1,32 +1,20 @@
 # Clean IndCol data
-# 9.21.21 SP
+# 9.28.21 SP
 
 # load required packages
 library(tidyverse)
+library(here)
 
 # load source functions
-source('/Users/ssp160230/github_directory/IndCol_Restructure/src/IndCol_functions.R')
+source(here::here('src', 'IndCol_functions.R'))
 
-# set hard-coded variables 
-ic_raw <- read_csv('/Users/ssp160230/github_directory/IndCol_Restructure/data/IndCol_data.csv')
+# set hard-coded variables
 
-# intermediate clean dfs
+# load data
+ic_raw <- read_csv(here::here('data', 'IndCol_data.csv'))
+
+# clean and reformat raw df
 ic_clean <- icclean(ic_raw)
 
-# intermediate mean df
+# calculate mean scores for each subscale
 ic_mean <- icmean(ic_clean)
-
-# write csv for clean df
-write.csv(
-  ic_clean, 
-  "/Users/ssp160230/github_directory/IndCol_Restructure/output/IndCol_clean.csv", 
-  row.names = FALSE
-)
-
-# write csv for octant df
-write.csv(
-  ic_mean, 
-  "/Users/ssp160230/github_directory/IndCol_Restructure/output/IndCol_mean.csv", 
-  row.names = FALSE
-)
-
